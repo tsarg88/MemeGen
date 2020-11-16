bc59ad1e735f387d2f10a88a164b1bdbf697bd4a
umbur <work1415@gmail.com>
2020-11-15 21:22:42 -0700

    creates memegenerator class component with initial setup

---
+++ memegen/src/MemeGenerator.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class MemeGenerator extends React.Component {
+  constructor() {
+    super();
+    this.state({});
+  }
+  render() {
+    return <div></div>;
+  }
+}
+
+export default MemeGenerator;
